8db8a0fb31d38e31fb09046ef77f94b9a337e520
scenaristeur <scenaristeur@gmail.com>
2023-06-16 14:56:07 +0200

    typehead mode checkbox

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -25,7 +25,7 @@
 
         <b-form-input v-else class="sm-8 md-4" autofocus autocomplete="off" placeholder="What needs to be done?"
             v-model="newTodo" @keyup.enter="addTodo" />
-            <b-form-checkbox  style="padding-left:20px;"  v-model="typehead">
+            <b-form-checkbox  style="padding-left:20px;"  title="Check to use typehead autocomplete input. See more/help if needed"  v-model="typehead">
             </b-form-checkbox>
         </span>
 </template>
